1ad5b526ea792203692d15014f0d1fe4166c21de
Nathan Nifong <nhnifong@uncc.ath.cx>
2013-01-26 22:18:11 +0000

    game init

--- code/PuertoRico.py
+++ code/PuertoRico.py
@@ -1,7 +1,27 @@
+from random import randint, choice, shuffle
+
+resource = {
+    'empty:'0,
+    'indigo':1,
+    'corn':2,
+    'sugar':3,
+    'coffee':4,
+    'tobacco':5
+    'quarry':6
+}
 
 class PlayerMat:
     def __init__(self):
         self.doubloons = 0
+        self.isGoverner = False
+        # zero is empty. is this is the number of plantation tiles of each type you have
+        self.plantationTiles = {0:12, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+
+    def firstEmptyPlantation(self):
+        for i,tile in enumerate(self.plantationTiles):
+            if tile == 0:
+                return i
+        return None
 
     def serialize(self):
         flat = []
@@ -11,12 +31,37 @@ class PlayerMat:
 class Gamestate:
     def __init__(self,np):
         self.numPlayers = np
-        self.playerTurn = 0
-        self.playerMats = []
-        for i in range(self.numPlayers):
-            self.playerMats.append( PlayerMat() )
+        self.cp = randint(0, np-1) # short for current player
+        self.playerMats = [PlayerMat() for pm in range(np)]
+        self.playerMats[self.cp].isGoverner = True
+        for i,mat in enumerate(self.playerMats):
+            mat.doubloons = np-1
+            # first half of players get indigo, second half get corn
+            mat.plantationTiles[ firstEmptyPlantation() ] = (i-cp+np)%np*2//np+1
+        self.freeVictoryPoints = np*25
+        self.freeQuarryTiles = 8
+        # upside down shuffled plantation tiles
+        self.hiddenTiles = []
+        self.hiddenTiles.extend([resource['coffee']] * 8 )
+        self.hiddenTiles.extend([resource['tobacco']]* 9 )
+        self.hiddenTiles.extend([resource['corn']]   * 10)
+        self.hiddenTiles.extend([resource['sugar']]  * 11 )
+        self.hiddenTiles.extend([resource['indigo']] * 12 )
+        self.hiddenTiles.shuffle()
+        self.knownTiles = {1:0, 2:0, 3:0, 4:0, 5:0}
+        numProspectors = max(np-3,0)
+        self.ships = [[0]*np+1+i for i in range(3)]
+        # number of each type available
+        self.freegoods = {
+            resource['coffee']: 9,
+            resource['tobacco']: 9,
+            resource['corn']: 10,
+            resource['sugar']: 11
+            resource['indigo']: 11,
+        }
 
-    def serialize(self):
+    def serialize(self, perspective):
+        # perspective is a given player number, i.e, yourself.
         flat = []
         flat.append(float(self.numPlayers))
         flat.append(float(self.playerTurn))
